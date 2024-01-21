df4395d24f86466bd62168f4cf383ec63e94017e
laxmanpokhrel <laxmanpokhrel102@gmail.com>
2024-01-21 14:19:43 +0545

    fix: ts error for incompatible error component

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 
 export type ErrorComponentProps = { fileName: string; errorLocation: string; errorMessage: string | undefined };
-export type ErrorComponentType = (props?: ErrorComponentProps) => JSX.Element;
+export type ErrorComponentType = (props: ErrorComponentProps) => JSX.Element;
 
 export type ContextValueType = { errorComponent?: ErrorComponentType };
